78be9591a7ee1cf24f2c3c5107e78f7ffa2a9b8f
RahulAid <raccess97@gmail.com>
2022-10-23 12:48:40 +0530

    Program to Reverse a Number

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace BLZLogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be Reversed");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int num = n;
+            int rev = 0;
+            while (n != 0)
+            {
+                int d = n % 10;
+                rev = rev * 10 + d;
+                n /= 10;
+            }
+            Console.WriteLine("Reverse of " + num + " is " + rev);
+        }
+    }
+}
