632174cbcec4fe96f57fa14088f8f12ad535d68f
RahulAid <raccess97@gmail.com>
2022-10-31 02:40:02 +0530

    Stopwatch Problem

--- Program.cs
+++ Program.cs
@@ -1,33 +1,22 @@
-﻿using System.Diagnostics;
-
-namespace BLZLogicalPrograms
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class StopwatchProgram
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Stopwatch stopwatch = new Stopwatch();
+        Console.WriteLine("Start");
+        stopwatch.Start();
+
+        for (int i = 0; i < 2000; i++)
         {
-
-            Console.WriteLine("Enter Nth term of the Fibonacci Series : ");
-            int N = Convert.ToInt32(Console.ReadLine());
-
-            int a = 0;
-            int b = 1;
-            int c;
-
-            Console.WriteLine("The Fibonacci Series for "+N+" terms are :");
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-
-            for (int i = 0; i < (N - 2); i++)
-            {
-                c = a + b;
-                Console.WriteLine(c);
-                a = b;
-                b = c;
-            }
-            Console.ReadLine();
-
-        
+            Thread.Sleep(30);
         }
+
+        Console.WriteLine("Stop");
+        stopwatch.Stop();
+
+        Console.WriteLine("Time Elapsed : {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
     }
 }
